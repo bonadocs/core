18f46749675d79b906506eb9db88c987df56beb6
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-22 22:12:12 +0100

    Create abstract code runner for code execution

---
+++ src/lib/codesandbox/types.ts
@@ -0,0 +1,13 @@
+﻿export abstract class CodeRunner {
+  readonly #language: string
+
+  protected constructor(language: string) {
+    this.#language = language
+  }
+
+  get language() {
+    return this.#language
+  }
+
+  abstract run(code: string): Promise<unknown>
+}

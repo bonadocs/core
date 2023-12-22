0a9fc6a94d3139e2dd4f01172ab91efd41a42282
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2023-12-22 22:15:24 +0100

    Implement JS code runner resolution based on runtime environment

---
+++ src/lib/codesandbox/languages/js/index.ts
@@ -0,0 +1,20 @@
+﻿import { JavaScriptCodeRunner } from './base'
+
+let codeRunner: JavaScriptCodeRunner | undefined
+
+export async function getCodeRunner(): Promise<JavaScriptCodeRunner> {
+  if (codeRunner) {
+    return codeRunner
+  }
+
+  if (typeof window !== 'undefined' && typeof window.Worker !== undefined) {
+    const { BrowserJSCodeRunner } = await import('./browser')
+    codeRunner = new BrowserJSCodeRunner()
+  } else if (typeof process === 'object' && process?.versions?.node) {
+    const { NodeJSRunner } = await import('./node')
+    codeRunner = new NodeJSRunner()
+  } else {
+    throw new Error('Unsupported environment for running JavaScript code')
+  }
+  return codeRunner
+}

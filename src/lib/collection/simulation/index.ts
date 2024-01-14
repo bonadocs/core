f2847db2a6a59c58a851c4cabe2c8884af28accf
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-01-15 00:36:35 +0100

    Load raw error data in simulation response
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

--- src/lib/collection/simulation/index.ts
+++ src/lib/collection/simulation/index.ts
@@ -1,5 +1,9 @@
-﻿import { getApi } from '../../api'
+﻿import { isCallException } from 'ethers'
+
+import { getApi } from '../../api'
+import { getProvider } from '../execution/util'
 import { ExecutableEVMCall } from '../spec'
+import { ExecutionResultData } from '../types'
 
 /**
  * This class is maintained separately rather than just relying on the API object because
@@ -7,12 +11,68 @@ import { ExecutableEVMCall } from '../spec'
  * a central API service to process simulations.
  */
 export class TransactionSimulator {
-  async simulateBundle(chainId: number, calls: ExecutableEVMCall[]) {
+  async simulateBundle(
+    chainId: number,
+    calls: ExecutableEVMCall[],
+  ): Promise<ExecutionResultData[] | undefined> {
     const api = getApi()
     if (!api) {
       throw new Error('Simulation API must be setup')
     }
 
-    return await api.simulateEVMBundle(chainId, calls)
+    const responses = await api.simulateEVMBundle(chainId, calls)
+    if (!responses) {
+      return responses
+    }
+
+    const results: ExecutionResultData[] = []
+    for (const response of responses) {
+      if (!response.error) {
+        results.push(response.receipt)
+        continue
+      }
+
+      results.push({
+        ...response.receipt,
+        error: response.error,
+      })
+    }
+
+    if (results.length === 1 && results[0].error && !results[0].error.data) {
+      results[0].error.data =
+        (await this.#tryGetErrorData(chainId, calls[0])) || undefined
+    }
+    return results
+  }
+
+  /**
+   * Implemented only for a single call case. If there are multiple calls, we cannot reliably re-create
+   * the state of the EVM to get the error data until we have a full simulation API.
+   *
+   * @param chainId
+   * @param call
+   * @private
+   */
+  async #tryGetErrorData(
+    chainId: number,
+    call: ExecutableEVMCall,
+  ): Promise<string | null> {
+    try {
+      const provider = await getProvider(null, chainId)
+      await provider.call({
+        from: call.overrides.from,
+        to: call.to,
+        data: call.data,
+        chainId,
+        value: call.overrides.value,
+        gasLimit: call.overrides.gasLimit,
+        gasPrice: call.overrides.gasPrice,
+      })
+    } catch (e) {
+      if (isCallException(e)) {
+        return e.data
+      }
+    }
+    return null
   }
 }

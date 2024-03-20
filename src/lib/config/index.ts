e2f2781705d666238b86b15a32268aa6a6f74599
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2024-03-20 18:33:45 +0800

    Update bonadocs API URL
    Signed-off-by: Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,6 +1,5 @@
 export const config = {
-  defaultAPIUrl:
-    'https://v7sbpz4erztmg3mb2ci6q4ctte0xhpne.lambda-url.eu-central-1.on.aws',
+  defaultAPIUrl: 'https://api.bonadocs.com',
   nftStorageApiKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJDODU4YzY4MWJkNkFEZkZEZDc3MTVERTBjMjJhMjI1RTIwNzhjQ2MiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0NjcxNzk2MzM4MiwibmFtZSI6ImNoYWlud2FsbGV0LWRldiJ9.O4mii7yVpdMWumt19uJTwzg2ccTOwrwCFuuhl0Ge8Gk',
 }
